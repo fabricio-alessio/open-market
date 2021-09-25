83333165cde611207c55ada3ad0e4cd526c8c335
Fabricio Alessio <fabricio.alessio@ifood.com.br>
2021-09-25 19:33:45 -0300

    Add log

--- src/main/java/com/unico/openmarket/market/MarketService.java
+++ src/main/java/com/unico/openmarket/market/MarketService.java
@@ -9,6 +9,8 @@ import com.unico.openmarket.subcityhall.SubCityHall;
 import com.unico.openmarket.subcityhall.SubCityHallAdapter;
 import com.unico.openmarket.subcityhall.SubCityHallDto;
 import com.unico.openmarket.subcityhall.SubCityHallRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -17,6 +19,8 @@ import java.util.Optional;
 
 @Service
 public class MarketService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarketService.class);
 
     private MarketRepository repository;
     private DistrictRepository districtRepository;
@@ -30,6 +34,7 @@ public class MarketService {
 
     public Optional<MarketDto> create(MarketDto marketDto) {
 
+        LOGGER.debug("Try to create new market {}", marketDto);
         return repository.findByCode(marketDto.getCode())
                 .map(foundMarket -> {
                     return update(marketDto.getCode(), marketDto);
@@ -38,6 +43,7 @@ public class MarketService {
 
     private Optional<MarketDto> createNew(MarketDto marketDto) {
 
+        LOGGER.debug("Create new market {}", marketDto);
         final var district = createNewDistrictIfDoesNotExists(marketDto.getDistrict());
         final var subCityHall = createNewSubCityHallIfDoesNotExists(marketDto.getSubCityHall());
 
@@ -49,12 +55,14 @@ public class MarketService {
 
     private District createNewDistrictIfDoesNotExists(DistrictDto districtDto) {
 
+        LOGGER.debug("Create new district if not exists {}", districtDto);
         return districtRepository.findByCode(districtDto.getCode())
                 .orElseGet(() -> districtRepository.save(DistrictAdapter.adaptDistrict(districtDto)));
     }
 
     private SubCityHall createNewSubCityHallIfDoesNotExists(SubCityHallDto subCityHallDto) {
 
+        LOGGER.debug("Create new sub city hall if not exists {}", subCityHallDto);
         return subCityHallRepository.findByCode(subCityHallDto.getCode())
                 .orElseGet(() -> subCityHallRepository.save(SubCityHallAdapter.adaptSubCityHall(subCityHallDto)));
     }
@@ -64,8 +72,9 @@ public class MarketService {
         return Collections.emptyList();
     }
 
-    private MarketDto generateMarket(Market market) {
+    private MarketDto generateMarketDto(Market market) {
 
+        LOGGER.debug("Generate a marketDto from a market {}", market);
         final var districtOpt = districtRepository.findById(market.getDistrictId());
         if (!districtOpt.isPresent()) {
             throw new EntityNotFoundException("District id " + market.getDistrictId() + " not found!");
@@ -82,14 +91,16 @@ public class MarketService {
 
     public Optional<MarketDto> findByCode(long code) {
 
+        LOGGER.debug("Find market by code {}", code);
         return repository.findByCode(code)
                 .map(record -> {
-                    return Optional.of(generateMarket(record));
+                    return Optional.of(generateMarketDto(record));
                 }).orElse(Optional.empty());
     }
 
     public Optional<MarketDto> update(long code, MarketDto marketDto) {
 
+        LOGGER.debug("Update market code {} - {}", code, marketDto);
         return repository.findByCode(code)
                 .map(record -> {
                     final var district = createNewDistrictIfDoesNotExists(marketDto.getDistrict());
@@ -117,6 +128,7 @@ public class MarketService {
 
     public boolean deleteByCode(long code) {
 
+        LOGGER.debug("Delete market code {}", code);
         return repository.findByCode(code)
                 .map(record -> {
                     repository.deleteById(record.getId());
